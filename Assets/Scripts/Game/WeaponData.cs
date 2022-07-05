a0b4697c8b01b7d9f16a9e593d67af73a9e851d3
Rustam Mamedov <mamedov@panzerdog.com>
2022-07-05 14:37:21 +0200

    Added project assets

---
+++ Assets/Scripts/Game/WeaponData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Game {
+
+    public class WeaponData : ScriptableObject {
+
+        public string nameLocalizationKey;
+        public int damage;
+        public GameObject geometry;
+    }
+}
